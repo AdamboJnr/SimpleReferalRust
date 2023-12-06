cb3c7418922b4234bee8159c144b2338cd584e35
AdamboJnr <AdamboAllan75@gmail.com>
2023-12-06 23:12:15 +0300

    Added check_balance function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+#![allow(unused_imports)]
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -7,13 +8,14 @@ use std::rc::Rc;
 pub struct Referal {
     name: String,
     children: RefCell<Vec<Rc<Referal>>>,
+    commission: RefCell<i64>,
 }
 
 impl Referal {
     pub fn new(name: String, users: &CreatedUsers) -> Rc<Referal> {
-       let new_referal = Rc::new(Referal { name , children: RefCell::new(vec![]) });
+       let new_referal = Rc::new(Referal { name , children: RefCell::new(vec![]), commission: RefCell::new(0) });
 
-       users.add_user(new_referal);
+       users.add_user(Rc::clone(&new_referal));
 
         new_referal
     }
@@ -21,8 +23,17 @@ impl Referal {
     pub fn add_child(&self, child: Rc<Referal>) {
         self.children.borrow_mut().push(Rc::clone(&child))
     }
+
+    pub fn check_balance(&self) -> &RefCell<i64> {
+        if self.children.borrow().len() >= 4 {
+            *self.commission.borrow_mut() += 400
+        }
+
+        &self.commission
+    }
 }
 
+#[derive(Debug)]
 pub struct CreatedUsers {
     users: RefCell<Vec<Rc<Referal>>>
 }
@@ -33,7 +44,7 @@ impl CreatedUsers {
     }
 
     pub fn add_user(&self, user: Rc<Referal>) {
-        self.users.borrow_mut().push(Rc::clone(&user));
+        self.users.borrow_mut().push(user);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 extern crate referals;
 
 use referals::{ Referal, CreatedUsers };
+use std::rc::Rc;
 fn main() {
     let users = CreatedUsers::new();
 
     let parent = Referal::new(String::from("Allan"), &users);
 
     let child = Referal::new(String::from("Levy"), &users);
+    let child2 = Referal::new(String::from("Levy"), &users);
+    let child3 = Referal::new(String::from("Levy"), &users);
+    let child4 = Referal::new(String::from("Levy"), &users);
 
     parent.add_child(child);
+    parent.add_child(child2);
+    parent.add_child(child3);
+    parent.add_child(child4);
 
     println!("{:?}", parent);
+    println!("{:?}", Rc::strong_count(&parent));
+    println!("{:?}", users);
+    println!("{:?}", parent.check_balance());
 }
